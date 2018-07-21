51772767cead47a07d43dc05d1b168ca777b57c7
matheusbsilva <matheusbattista@hotmail.com>
2018-07-21 15:15:39 -0300

    Handle errors when Encode image

--- qrcoded_test.go
+++ qrcoded_test.go
@@ -4,7 +4,23 @@ import (
     "testing"
     "bytes"
     "image/png"
+    "errors"
 )
+
+type ErrorWriter struct{}
+
+func (e *ErrorWriter) Write(b []byte) (int, error) {
+    return 0, errors.New("Expected error")
+}
+
+func TestGenerateQRCodePropagatesErrors(t *testing.T) {
+    w := new(ErrorWriter)
+    err := GenerateQRCode(w, "555-32432")
+
+    if err == nil || err.Error() != "Expected error" {
+        t.Errorf("Error not propagated correctly, go %v", err)
+    }
+}
 
 func TestGenerateQRCodeGeneratesPNG(t *testing.T) {
     buffer := new(bytes.Buffer)
